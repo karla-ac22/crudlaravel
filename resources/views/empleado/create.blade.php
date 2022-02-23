33316d062ccf84e179710621f8d36c834d0ffd1e
karla-ac22 <karlyalta@gmail.com>
2022-02-23 16:29:00 -0500

    interfaz pt 2

--- resources/views/empleado/create.blade.php
+++ resources/views/empleado/create.blade.php
@@ -1,6 +1,11 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
 <h1>Formulario Creación Empleados</h1>
 <form action="{{url('/empleado')}}" method="post" enctype="multipart/form-data">
     @csrf 
     @include('empleado.form')
     
 </form>
+</div>
+@endsection
--- resources/views/empleado/edit.blade.php
+++ resources/views/empleado/edit.blade.php
@@ -1,6 +1,11 @@
+@extends('layouts.app')
+@section('content')
+<div class="container">
 <h1>Editar información de empleado</h1>
 <form action="{{url('/empleado/'.$persona->id)}}" method="post" enctype="multipart/form-data">
     @csrf 
     {{method_field('POST')}}
     @include('empleado.form')
 </form>
+</div>
+@endsection
